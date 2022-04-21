7fa79cdfbac7a8cf9547074cd90c29f58f535aac
OctoPokto <fj.anders@gmail.com>
2022-04-21 13:55:40 +0200

    Started camera movement mechanic
    Added test BG and started on the script for movement

---
+++ Barista/Assets/Scripts/Camera/MoveCamera.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Funksoft.Barista
+{
+    public class CameraMovement : MonoBehaviour
+    {
+        [SerializeField]
+        private AnimationCurve camMove;
+
+        private Vector3 orgPos;
+        private Vector3 targetPos;
+
+
+        void Start()
+        {
+        
+        }
+
+        void FixedUpdate()
+        {
+        
+        }
+    }
+}
