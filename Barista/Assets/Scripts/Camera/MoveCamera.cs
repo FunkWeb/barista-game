0e59a6c8bf2c0c77ab59829e68d866d6feb02d2a
OctoPokto <fj.anders@gmail.com>
2022-04-22 12:49:33 +0200

    Camera movement done
    It works!

--- Barista/Assets/Scripts/Camera/MoveCamera.cs
+++ Barista/Assets/Scripts/Camera/MoveCamera.cs
@@ -4,23 +4,62 @@ using UnityEngine;
 
 namespace Funksoft.Barista
 {
-    public class CameraMovement : MonoBehaviour
+    public class MoveCamera : MonoBehaviour
     {
         [SerializeField]
         private AnimationCurve camMove;
 
-        private Vector3 orgPos;
-        private Vector3 targetPos;
+        private Vector3 posA;
+        private Vector3 posB;
 
+        [SerializeField]
+        private float duration;
 
-        void Start()
+        void Awake()
         {
-        
+            //Hard coding positions because they don't need to be changed once set.
+            //Might ned to adjust 19.2 based on background size. As well as camera projections size 5.4.
+            posA = transform.localPosition;
+            posB = new Vector3(transform.localPosition.x + 19.2f, 0, -10);
+            
         }
 
-        void FixedUpdate()
+        void Update()
         {
-        
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                Debug.Log("Right arrow presed");
+                StartCoroutine(SwitchPos(true));
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                Debug.Log("Left arrow presed");
+                StartCoroutine(SwitchPos(false));
+            }
+            
+        }
+
+        //lerps the camera position to position A or B depending on current location.
+        private IEnumerator SwitchPos(bool isPosA)
+        {
+            Debug.Log("SwitchPos started");
+
+            float time = 0;
+
+            while (time < duration)
+            {
+                if(isPosA)
+                {
+                    transform.position = Vector3.Lerp(posA, posB, camMove.Evaluate(time));
+                }
+                else
+                {
+                    transform.position = Vector3.Lerp(posB, posA, camMove.Evaluate(time));
+                }
+                time += Time.deltaTime;
+                yield return null;
+            }
         }
     }
 }
